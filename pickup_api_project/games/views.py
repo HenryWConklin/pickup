d96e29e3799b4b44187fec07967d52285c103dd1
lmerrick <lsm3bn@virginia.edu>
2015-02-28 18:56:49 -0500

    got test page working

--- pickup_api_project/games/views.py
+++ pickup_api_project/games/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def test(request):
+    build_test_db()
+
+    return HttpResponse("TEST RESPONSE")
+
+def get_full_game_list(request):
+    #fill in
+    return HttpResponse("")
+
+def build_test_db():
+    #fill in
--- pickup_api_project/pickup_api_project/urls.py
+++ pickup_api_project/pickup_api_project/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^games/', include('games.urls')),
 )
